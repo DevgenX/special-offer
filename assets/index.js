7f06173c746a930f668ecebc73f668054dcfee7e
devgenx <sebastian110420@gmail.com>
2023-05-22 16:36:58 -0700

    final version

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,10 @@
 const submitButton = document.getElementById("submit-btn");
 const getContainerDiv = document.getElementById("container");
 
+const getName = document.getElementById("name");
+const getEmail = document.getElementById("email");
+const getHandle = document.getElementById("handle");
+
 submitButton.addEventListener("click", () => {
   getContainerDiv.classList.remove("container");
   getContainerDiv.classList.add("change-bg");
@@ -10,6 +14,17 @@ submitButton.addEventListener("click", () => {
     submitButton.classList.add("disabled");
     submitButton.textContent = "Submitted";
   }
+
+  // setTimeout(() => {
+  //   // Assuming API call is happening
+  //   // Show alert when API call succeeds
+  //   alert("Form submitted successfully");
+  //   getContainerDiv.classList.remove("change-bg");
+  //   submitButton.classList.remove("disabled");
+  //   getContainerDiv.classList.add("container");
+  //   submitButton.disabled = false;
+  //   submitButton.textContent = "Submit";
+  // }, 1500);
 
   // toggle functionality
   //   getContainerDiv.classList.toggle("container");
